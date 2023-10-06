96dca8ce4a41793a4b9c61af4563b1d71ce4c465
obedgiu200 <145864457+obedgiu200@users.noreply.github.com>
2023-10-06 08:14:22 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+from high_pricing import high_pricing
+
 data=pd.read_csv('vehicles_us.csv')
 
 
@@ -11,25 +13,23 @@ st.subheader('Use this App to select your best car')
 
 
 
-st.caption('blue[choose your parameters here]')
+st.caption('red[choose your parameters here]')
 price_range = st.slider(
      'What is your price range?',
      value=(1,375000))
 
 actual_range=list(range(price_range[0],price_range[1]+1))
 
-high_pricing= st.checkbox('only high pricing')
-
 if high_pricing:
     filtered_data=[data.price.isin(actual_range)]
-    filtered_data=filtered_data[data.pricing>=250000]
+    filtered_data=filtered_data[data.price>=250000]
 else:
     filtered_data=data[data.price.isin(actual_range)]
 
-st.write('Here are your options with split by type and model')
+st.write('Here are your options with split by model')
 
 
-fig = px.scatter(filtered_data, x="type", y="model")
+fig = px.scatter(filtered_data, x="model")
 st.plotly_chart(fig)
 
 st.write('Determine the condition of filtered vehicles')
