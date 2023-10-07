763f14f9ba91c4798954bae3c06c6401b8a932ac
obedgiu200 <145864457+obedgiu200@users.noreply.github.com>
2023-10-07 09:18:39 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -9,11 +9,9 @@ st.title ('BUY YOUR BEST CAR!')
 st.subheader('Use this App to select your best car')
 
 
-
-
 st.caption('red[choose your parameters here]')
 price_range = st.slider(
-     'What is your price?',
+     'What is your price range?',
      value=(1,375000))
 
 actual_range=list(range(price_range[0],price_range[1]+1))
@@ -21,14 +19,14 @@ high_pricing = st.checkbox('only high pricing')
 
 if high_pricing:
     filtered_data=[data.price.isin(actual_range)]
-    filtered_data=filtered_data[data.price>=250000]
+    filtered_data=filtered_data[filtered_data.price>=250000]
 else:
-    filtered_data=[data.price.isin(actual_range)]
+    filtered_data=data[data.price.isin(actual_range)]
 
 
 st.write('Here are your options with split by type')
 
-fig = px.scatter(filtered_data, x='type',y='price')
+fig = px.scatter(filtered_data, x='type')
 st.plotly_chart(fig)
 
 st.write('Determine the condition of filtered vehicles')
